62b5deb288c8ca5d6fc801d13924fa27ab6e3e5d
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-04-27 20:55:11 +0200

    start

--- src/components/ResturanNavbar.tsx
+++ src/components/ResturanNavbar.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 function ResturanNavbar(): JSX.Element {
   return (
     <nav className="flex text-reg border-b pb-2">
-      <Link href="/dashboard/resturant/d" className="mr-7">
+      <Link href="/dashboard/restaurant/d" className="mr-7">
         Overview
       </Link>
-      <Link href="/dashboard/resturant/d/menu" className="mr-7">
+      <Link href="/dashboard/restaurant/d/menu" className="mr-7">
         Menu
       </Link>
     </nav>
